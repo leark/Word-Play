63ff9df8fa4f7e2e24fc952e1b894b11ebb28af5
Seung Lee <shl7@uw.edu>
2022-06-06 17:10:20 -0700

    Add comments in JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
 $(document).ready(function () {
   $('form#form-sentence').submit(function (event) {
     event.preventDefault();
+
+    // grabbing value from inside id sentence
     const sentence = $('#sentence').val();
-    let thereIsAPeriod = false;
-    if (sentence.charAt(sentence.length - 1) === '.') {
-      thereIsAPeriod = true;
-      // remove period from the string
-    }
+
+    // let thereIsAPeriod = false;
+    // if (sentence.charAt(sentence.length - 1) === '.') {
+    //   thereIsAPeriod = true;
+    //   // remove period from the string
+    // }
+
+    // split sentence by ' ' and put it in a new array
     const sentenceArray = sentence.split(' ');
 
+    // initialize new array
     const newArray = [];
+
+    // loop through and check for words that are bigger or equal to 3 letters and add them to initialized array
     sentenceArray.forEach(function (word) {
       if (word.length >= 3) {
         newArray.push(word);
       }
     });
+
+    // reverse the order of elements inside the array
     newArray.reverse();
+
+    // join the elements to make a new string
     let reverseJoined = newArray.join(' ');
+
     // reverseJoined += '.';
+
     $('#output').text(reverseJoined);
   });
 
