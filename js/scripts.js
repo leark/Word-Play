877109e3c131c9e660a18cb26e2dfa249517837c
Seung Lee <shl7@uw.edu>
2022-06-06 17:05:48 -0700

    Add clear button

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
-$(document).ready(function() {
-  $("form#form-sentence").submit(function(event) {
+$(document).ready(function () {
+  $('form#form-sentence').submit(function (event) {
     event.preventDefault();
-    const sentence = $("#sentence").val();
-   
-    const sentenceArray = sentence.split(" ");
-    
+    const sentence = $('#sentence').val();
+    let thereIsAPeriod = false;
+    if (sentence.charAt(sentence.length - 1) === '.') {
+      thereIsAPeriod = true;
+      // remove period from the string
+    }
+    const sentenceArray = sentence.split(' ');
+
     const newArray = [];
-    sentenceArray.forEach(function(word) {
+    sentenceArray.forEach(function (word) {
       if (word.length >= 3) {
         newArray.push(word);
       }
     });
     newArray.reverse();
-    const reverseJoined = newArray.join(" ");
-    $("#output").text(reverseJoined);
+    let reverseJoined = newArray.join(' ');
+    // reverseJoined += '.';
+    $('#output').text(reverseJoined);
+  });
+
+  $('#clear').click(function () {
+    $('#output').val('');
+    $('#sentence').val('');
   });
 });
-
